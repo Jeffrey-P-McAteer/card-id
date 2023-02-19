f66b60d390ae6a6f2ed8241a4208f0e84a93273b
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-02-18 20:01:06 -0500

    more APDU nonsense

--- src/main.cpp
+++ src/main.cpp
@@ -33,19 +33,62 @@ int main(int argc, char** argv) {
       else {
         std::cout << "card_ptr=" << card_ptr.get() << std::endl;
 
-        auto command = pcsc_cpp::CommandApdu::fromBytes({0x2, 0x1, 0x3, 0x4}); // ????
-        auto transactionGuard = card_ptr->beginTransaction();
-        auto response = card_ptr->transmit(command); // Returns ResponseApdu
+        // auto command = pcsc_cpp::CommandApdu::fromBytes({0x2, 0x1, 0x3, 0x4}); // ????
+        // auto transactionGuard = card_ptr->beginTransaction();
+        // auto response = card_ptr->transmit(command); // Returns ResponseApdu
 
-        //std::cout << "response=" << response.toBytes() << std::endl;
+        // //std::cout << "response=" << response.toBytes() << std::endl;
+        // auto resp_bytes = response.toBytes();
+        // std::string result{};
+        // result.reserve(4096);
+        // for (auto byte_b : resp_bytes) {
+        //   result += one_byte_to_string(byte_b);
+        // }
+
+        // std::cout << "response=" << result << std::endl;
+
+        // Get master file
+        auto masters_c = pcsc_cpp::CommandApdu::fromBytes({0x00, 0xa4, 0x00, 0x0c, /**/ 0x90, 0x00});
+        auto tx_guard = card_ptr->beginTransaction();
+        auto response = card_ptr->transmit(masters_c);
         auto resp_bytes = response.toBytes();
+
         std::string result{};
         result.reserve(4096);
         for (auto byte_b : resp_bytes) {
           result += one_byte_to_string(byte_b);
         }
+        std::cout << "Master File result=" << result << std::endl;
 
-        std::cout << "response=" << result << std::endl;
+        // Select directory
+        auto directory_c = pcsc_cpp::CommandApdu::fromBytes({0x00, 0xa4, 0x01, 0x0c, 0x02, 0xee, 0xee, /**/ 0x90, 0x00});
+        auto tx_guard2 = card_ptr->beginTransaction();
+        response = card_ptr->transmit(directory_c);
+        resp_bytes = response.toBytes();
+
+        std::string result2{};
+        result2.reserve(4096);
+        for (auto byte_b : resp_bytes) {
+          result2 += one_byte_to_string(byte_b);
+        }
+        std::cout << "Directory result2=" << result2 << std::endl;
+
+        // Select certificate file
+        auto certificate_c = pcsc_cpp::CommandApdu::fromBytes({0x00, 0xa4, 0x02, 0x0c, 0x02, 0xaa, 0xce, /**/ 0x90, 0x00});
+        auto tx_guard3 = card_ptr->beginTransaction();
+        response = card_ptr->transmit(certificate_c);
+        resp_bytes = response.toBytes();
+
+        std::string result3{};
+        result3.reserve(4096);
+        for (auto byte_b : resp_bytes) {
+          result3 += one_byte_to_string(byte_b);
+        }
+        std::cout << "Certificate result3=" << result3 << std::endl;
+
+        
+
+
 
 
       }
