1e5747460ac1e00c63e174943038c3a105ccd59f
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-02-20 08:37:57 -0500

    got stuff back!

--- src/main.cpp
+++ src/main.cpp
@@ -52,32 +52,44 @@ int main(int argc, char** argv) {
         //   std::cout << "req=" << bytes_to_str(req) << " resp=" << bytes_to_str(resp) << std::endl;
         // }
 
-        // We want to try to read everything; when we get back 0x90 (OK) or 0x61 (more data available) as first byte of response we found something!
-        for (unsigned char p1=0x00; p1 < 0xff; p1 += 1) {
-          std::cout << "p1=" << one_byte_to_string(p1) << std::endl;
-          for (unsigned char p2=0x00; p2 < 0x0f; p2 += 1) {
-            std::vector<unsigned char> req {
-              0x00, // Class byte, always 0
-              0xB2, // Instruction: 0xA4 means Select (something), 0xB2 means read a record
-              p1, // Instruction parameter 1
-              p2, // Instruction parameter 2
-              0x00, // number of data bytes (next N bytes)
-              // No data bytes
-              0x00, // Max number of response bytes we can accept; 0 == 256
-            };
-            try {
-              auto resp = do_tx(card_ptr, req);
-              if (resp[0] == 0x90 || resp[0] == 0x61) {
-                std::cout << "p1=" << one_byte_to_string(p1) << " p2=" << one_byte_to_string(p2) << std::endl;
-                std::cout << "req=" << bytes_to_str(req) << " resp=" << bytes_to_str(resp) << std::endl;
-                std::cout << std::endl;
-              }
-            }
-            catch (...) {
-              // Guess who doesn't care?
-            }
-          }
-        }
+        std::vector<unsigned char> fcp_req {
+          0x00, // Class byte, always 0
+          0xA4, // Instruction: 0xA4 means Select (something), 0xB2 means read a record
+          0x04, // Instruction parameter 1
+          0x00, // Instruction parameter 2
+          0x07, // number of data bytes (next N bytes)
+          0xA0, 0x00, 0x00, 0x00, 0x03, 0x10, 0x10, // VISA AID
+          // 0x00, // Max number of response bytes we can accept; 0 == 256
+        };
+        auto fcp_resp = do_tx(card_ptr, fcp_req);
+        std::cout << "fcp_req=" << bytes_to_str(fcp_req) << " fcp_resp=" << bytes_to_str(fcp_resp) << std::endl;
+        std::cout << std::endl;
+
+        // // We want to try to read everything; when we get back 0x90 (OK) or 0x61 (more data available) as first byte of response we found something!
+        // for (unsigned char p1=0x00; p1 < 0xff; p1 += 1) {
+        //   for (unsigned char p2=0x00; p2 < 0x0f; p2 += 1) {
+        //     std::vector<unsigned char> req {
+        //       0x00, // Class byte, always 0
+        //       0xB2, // Instruction: 0xA4 means Select (something), 0xB2 means read a record
+        //       p1, // Instruction parameter 1
+        //       p2, // Instruction parameter 2
+        //       0x00, // number of data bytes (next N bytes)
+        //       // No data bytes
+        //       0x00, // Max number of response bytes we can accept; 0 == 256
+        //     };
+        //     try {
+        //       auto resp = do_tx(card_ptr, req);
+        //       if (resp[0] == 0x90 || resp[0] == 0x61) {
+        //         std::cout << "p1=" << one_byte_to_string(p1) << " p2=" << one_byte_to_string(p2) << std::endl;
+        //         std::cout << "req=" << bytes_to_str(req) << " resp=" << bytes_to_str(resp) << std::endl;
+        //         std::cout << std::endl;
+        //       }
+        //     }
+        //     catch (...) {
+        //       // Guess who doesn't care?
+        //     }
+        //   }
+        // }
 
 
 
